4618a8839de1e91a3498cd66b7432586804a4425
Justice <60824609+Justice57201@users.noreply.github.com>
2026-05-06 19:51:20 -0500

    Add files via upload

---
+++ options.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# By -- WRQC343 -- www.gmrs-link.com
+#
+# Ver 1.0 - 
+#
+
+FILE="/path/to/your/file"
+
+if [[ $EUID -ne 0 ]]; then
+    echo "Please run with sudo"
+    exit 1
+fi
+
+sudo rpi-rw
+
+if ! grep -q '^Options=UNIT=true$' "$FILE"; then
+    sudo sed -i '159i Options=UNIT=true' "$FILE"
+    echo "Line added."
+else
+    echo "Line already exists."
+fi
+
+sudo rpi-ro
